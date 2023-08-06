84734f039be15a13eb36ed61d21dd5cfaa2ee373
silva4dev <web4dev.lucas@outlook.com>
2023-08-05 22:06:38 -0300

    chore: ensure LoginRouter return a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -100,7 +100,7 @@ describe('Login Router', () => {
   })
 
   test('Should return 200 when valid credentials are provided', async () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
 
     const httpRequest = {
       body: {
@@ -111,6 +111,7 @@ describe('Login Router', () => {
 
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('Should return 500 if no AuthUseCase is provided', async () => {
