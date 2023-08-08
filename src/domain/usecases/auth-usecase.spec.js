113ddda0f5ed3287e8683f71cb9e3178f37a939d
silva4dev <web4dev.lucas@outlook.com>
2023-08-08 20:16:02 -0300

    chore: ensure AuthUseCase returns null if LoadUserByEmailRepository returns null

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -22,7 +22,11 @@ class AuthUseCase {
       throw new InvalidParamError('loadUserByEmailRepository')
     }
 
-    await this.loadUserByEmailRepository.load(email)
+    const user = await this.loadUserByEmailRepository.load(email)
+
+    if (!user) {
+      return null
+    }
   }
 }
 
@@ -75,5 +79,12 @@ describe('Auth UseCase', () => {
     const promise = sut.auth('any_email@mail.com', 'any_password')
 
     expect(promise).rejects.toThrow(new InvalidParamError('loadUserByEmailRepository'))
+  })
+
+  it('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+
+    expect(accessToken).toBeNull()
   })
 })
