36dd4197bb3c0bcd7feb8a0687186de93d3bedfc
silva4dev <web4dev.lucas@outlook.com>
2023-08-11 21:03:56 -0300

    chore: ensure AuthUseCase returns null if an invalid password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -5,10 +5,12 @@ const makeSut = () => {
   class LoadUserByEmailRepositorySpy {
     async load (email) {
       this.email = email
+      return this.user
     }
   }
 
   const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  loadUserByEmailRepositorySpy.user = {}
   const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
   return {
     sut,
@@ -45,16 +47,24 @@ describe('Auth UseCase', () => {
     expect(promise).rejects.toThrow()
   })
 
-  it('Should throw if no LoadUserByEmailRepository has no load method', async () => {
+  it('Should throw if LoadUserByEmailRepository has no load method', async () => {
     const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@mail.com', 'any_password')
 
     expect(promise).rejects.toThrow()
   })
 
-  it('Should return null if LoadUserByEmailRepository returns null', async () => {
-    const { sut } = makeSut()
+  it('Should return null if an invalid email is provided', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    loadUserByEmailRepositorySpy.user = null
     const accessToken = await sut.auth('invalid_email@mail.com', 'any_password')
+
+    expect(accessToken).toBeNull()
+  })
+
+  it('Should return null if an invalid password is provided', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth('valid_email@mail.com', 'invalid_password')
 
     expect(accessToken).toBeNull()
   })
