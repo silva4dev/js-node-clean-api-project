c7f6e70ed9e3b304ab42b95742dfcdcfc1af610e
silva4dev <web4dev.lucas@outlook.com>
2023-08-14 20:53:19 -0300

    chore: ensure Encrypter throws if no params are provided

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const Encrypter = require('./encrypter')
+const { MissingParamError } = require('../errors')
 
 const makeSut = () => {
   return new Encrypter()
@@ -28,5 +29,12 @@ describe('Encrypter', () => {
 
     expect(bcrypt.value).toBe('any_value')
     expect(bcrypt.hash).toBe('hashed_value')
+  })
+
+  test('Should throw if no params are provided', async () => {
+    const sut = makeSut()
+
+    expect(sut.compare()).rejects.toThrow(new MissingParamError('value'))
+    expect(sut.compare('any_value')).rejects.toThrow(new MissingParamError('hash'))
   })
 })
