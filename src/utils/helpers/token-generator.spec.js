0e0e7fbd55258d5cfec194308b03730012226783
silva4dev <web4dev.lucas@outlook.com>
2023-08-15 18:55:30 -0300

    chore: ensure TokenGenerator throws if no id is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -11,6 +11,10 @@ class TokenGenerator {
       throw new MissingParamError('secret')
     }
 
+    if (!id) {
+      throw new MissingParamError('id')
+    }
+
     return jwt.sign(id, this.secret)
   }
 }
@@ -48,5 +52,12 @@ describe('Token Generator', () => {
     const promise = sut.generate('any_id')
 
     expect(promise).rejects.toThrow(new MissingParamError('secret'))
+  })
+
+  test('Should throw if no id is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.generate()
+
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
   })
 })
