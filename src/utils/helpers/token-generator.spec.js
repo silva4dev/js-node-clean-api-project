c17d34c19fe59f3a326877e0b8158ecede0d37c3
silva4dev <web4dev.lucas@outlook.com>
2023-08-15 18:53:00 -0300

    chore: ensure TokenGenerator throws if no secret is provided

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const { MissingParamError } = require('../errors')
 
 class TokenGenerator {
   constructor (secret) {
@@ -6,6 +7,10 @@ class TokenGenerator {
   }
 
   async generate (id) {
+    if (!this.secret) {
+      throw new MissingParamError('secret')
+    }
+
     return jwt.sign(id, this.secret)
   }
 }
@@ -36,5 +41,12 @@ describe('Token Generator', () => {
 
     expect(jwt.id).toBe('any_id')
     expect(jwt.secret).toBe(sut.secret)
+  })
+
+  test('Should throw if no secret is provided', async () => {
+    const sut = new TokenGenerator()
+    const promise = sut.generate('any_id')
+
+    expect(promise).rejects.toThrow(new MissingParamError('secret'))
   })
 })
