60e1262c1db808a969e25e3ced3619bd7c246806
silva4dev <web4dev.lucas@outlook.com>
2023-08-19 19:46:26 -0300

    refactor: move cors to its own file

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,11 +1,7 @@
+const { cors } = require('../middlewares')
+
 module.exports = app => {
   app.disable('x-powered-by')
 
-  app.use((request, response, next) => {
-    response.set('access-control-allow-origin', '*')
-    response.set('access-control-allow-methods', '*')
-    response.set('access-control-allow-headers', '*')
-
-    next()
-  })
+  app.use(cors)
 }
---
+++ src/main/middlewares/cors.js
@@ -0,0 +1,7 @@
+module.exports = (request, response, next) => {
+  response.set('access-control-allow-origin', '*')
+  response.set('access-control-allow-methods', '*')
+  response.set('access-control-allow-headers', '*')
+
+  next()
+}
