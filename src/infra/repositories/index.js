c6280a8f2067cdbdc4f5e0b80c92874c1725f1de
silva4dev <web4dev.lucas@outlook.com>
2023-08-19 18:59:54 -0300

    refactor: move UpdateAccessTokenRepository to its own file

--- src/infra/repositories/index.js
+++ src/infra/repositories/index.js
@@ -1,5 +1,7 @@
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('./update-access-token-repository')
 
 module.exports = {
-  LoadUserByEmailRepository
+  LoadUserByEmailRepository,
+  UpdateAccessTokenRepository
 }
