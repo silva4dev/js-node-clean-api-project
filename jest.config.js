60ce0fdaf7683d0e40dba167a461aa9db6cb0c31
silva4dev <web4dev.lucas@outlook.com>
2023-12-11 21:33:01 -0300

    chore: ensure change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   coverageDirectory: 'coverage',
+  roots: ['<rootDir>/src'],
   testEnvironment: 'node',
+  collectCoverage: true,
   collectCoverageFrom: ['**/src/**/*.js', '!**/src/main/**'],
   preset: '@shelf/jest-mongodb',
   watchPathIgnorePatterns: ['globalConfig']
